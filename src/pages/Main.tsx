e21a9868322cb8ecbc66c0d1bf3d93b65d21254a
Maksim Khirug <maxkhirug@gmail.com>
2024-03-23 15:53:45 +0300

    Добавлен кот

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,6 +5,23 @@ const Main: FC = () => {
     return (
         <>
             <h1>Главная</h1>
+
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '80vh'
+                }}
+            >
+                <img
+                    src='../../public/cat-lightsaber.jpg'
+                    alt=""
+                    width="auto"
+                    height="100%"
+                />
+            </div>
+
         </>
     )
 }
